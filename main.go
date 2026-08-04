c66dd35943912ee5460094c41d71167331a7e438
alt-potato <jason.why.zhuang@gmail.com>
2026-08-04 17:08:39 -0400

    feat: add demo rendering script
    what kinda sidequest is this

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
 
 	"alt-potato/wordle-shuffler/internal/tui"
 
@@ -11,16 +14,90 @@ import (
 
 func main() {
 	termW, termH, _ := term.GetSize(int(os.Stdout.Fd()))
+
+	// signal handeler for cmd-c exit
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+
 	screen := tui.NewScreen(termW, termH)
 
-	screen.SetCell(0, 0, 'h', -1, -1)
-	screen.SetCell(1, 1, 'i', -1, -1)
-	screen.SetCell(3, 3, 'c', -1, -1)
-	screen.SetCell(4, 4, 'h', -1, -1)
-	screen.SetCell(5, 5, 'a', -1, -1)
-	screen.SetCell(6, 6, 't', -1, -1)
+	// start tick loop
+	const fps = 12
+	ticker := time.NewTicker(time.Second / fps)
+	defer ticker.Stop()
 
-	screen.Render()
+	frameCount := 0
+	// startTime := time.Now()
 
-	fmt.Scanln()
+main:
+	for {
+		select {
+		case <-sigCh:
+			break main
+		case <-ticker.C:
+			frameCount++
+
+			setDemo(screen, frameCount)
+
+			screen.Render()
+		}
+	}
+}
+
+func setDemo(screen *tui.Screen, tick int) {
+	// probably a better way to do this but whatever
+	type coord struct {
+		row, col int
+	}
+
+	vectors := [...]struct {
+		facing, length int // facing: NESW -> 0123 (too lazy to define in code)
+	}{
+		{1, 5},
+		{2, 5},
+		{3, 5},
+		{0, 5},
+	}
+
+	pathLength := 0
+	for _, v := range vectors {
+		pathLength += v.length
+	}
+
+	path := make([]coord, pathLength)
+
+	// build path (starting position is NOT counted (easier that way lol))
+	cursor := coord{2, 1}
+	curr := 0
+	for _, v := range vectors {
+	draw:
+		for range v.length {
+			switch v.facing {
+			case 0:
+				cursor.row -= 1 // north
+			case 1:
+				cursor.col += 1 // east
+			case 2:
+				cursor.row += 1 // south
+			case 3:
+				cursor.col -= 1 // west
+			default:
+				pathLength -= 1 // don't count invalid instruction
+				break draw // don't draw!
+				// should probably break out before the draw loop but whatever
+			}
+
+			path[curr] = cursor
+			curr++
+		}
+	}
+
+	snake := "******"
+	snake += strings.Repeat(" ", len(path)-len(snake)) // match length of path bc i'm lazy
+
+	pos := tick % len(path)
+	for _, char := range snake {
+		pos = (pos + 1) % len(path)
+		screen.SetCell(path[pos].row, path[pos].col, char, -1, -1)
+	}
 }
